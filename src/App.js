5f37fb37a03e3070e9036b7587bf6edd6f06d908
Ike Odira <odiraike8@gmail.com>
2022-08-12 16:21:49 +0100

    added some neccessary files

--- src/App.js
+++ src/App.js
@@ -2,18 +2,27 @@ import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Home from "./components/pages/Home";
+import Alert from "./components/layout/Alert";
 import About from "./components/pages/About";
 import NotFound from "./components/pages/NotFound";
+import { GithubProvider } from "./context/github/GithubContext";
+import { AlertProvider } from "./context/alert/AlertContext";
+import User from "./components/pages/User";
+
 
 function App() {
   return (
-    <Router>
+    <GithubProvider>
+      <AlertProvider>
+      <Router>
         <div className="flex flex-col justify-between h-screen">
           <Navbar/>
           <main className="container mx-auto px-3 pb-12">
+            <Alert/>
             <Routes>
                 <Route path="/" element={<Home/>}/>
                 <Route path="/about" element={<About/>}/>
+                <Route path="/user/:login" element={<User/>}/>
                 <Route path="/notfound" element={<NotFound/>}/>
                 <Route path="/*" element={<NotFound/>}/>
             </Routes>
@@ -21,6 +30,8 @@ function App() {
           <Footer/>
         </div>
     </Router>
+      </AlertProvider>    
+    </GithubProvider>
     );
 }
 
--- src/components/users/UserResults.js
+++ src/components/users/UserResults.js
@@ -1,32 +1,19 @@
 import Spinner from '../layout/Spinner';
-import {useEffect, useState} from 'react';
+import {useContext} from 'react';
 import UserItem from './UserItem';
+import GithubContext from '../../context/github/GithubContext';
 
 
 function UserResults(){
-    const [users, setUsers] = useState([]);
-    const [loading, setLoading] = useState(true);
-
-    useEffect(()=>{
-        fetchUsers();
-    }, [])
-
-    const fetchUsers = async ()=>{
-        const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`,{
-            headers:{
-                Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
-            }
-        })
-
-        const data = await response.json();
-        setUsers(data);
-        setLoading(false);
-    }
+    const {users, loading} = useContext(GithubContext);   
+    // useEffect(()=>{
+    //     fetchUsers();
+    // }, [])
 
     if(!loading){
         return(
             <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
-                {users.map((user)=>{
+                {users.map((user) => {
                     return(
                         <UserItem key={user.id} user={user}/>
                     )
@@ -38,8 +25,7 @@ function UserResults(){
             <Spinner/>
         )        
     }
-    
+     
 }
-
 
 export default UserResults;
